f48e2f36cb27b0589004f64e627c14337b5daf21
Rafael De Luca <deluca1712@gmail.com>
2021-04-06 18:47:07 -0700

    Adiciona modelo TagContent

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_06_144625) do
+ActiveRecord::Schema.define(version: 2021_04_07_012202) do
 
   create_table "contents", force: :cascade do |t|
     t.string "title", null: false
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2021_04_06_144625) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_contents_on_user_id"
+  end
+
+  create_table "tag_contents", force: :cascade do |t|
+    t.integer "tag_id", null: false
+    t.integer "content_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["content_id"], name: "index_tag_contents_on_content_id"
+    t.index ["tag_id"], name: "index_tag_contents_on_tag_id"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -43,5 +52,7 @@ ActiveRecord::Schema.define(version: 2021_04_06_144625) do
   end
 
   add_foreign_key "contents", "users"
+  add_foreign_key "tag_contents", "contents"
+  add_foreign_key "tag_contents", "tags"
   add_foreign_key "tags", "users"
 end
